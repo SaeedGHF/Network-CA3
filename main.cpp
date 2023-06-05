00d1a069157cc1b04567eec3d71bc5f1e6dfb61c
Saeed Ghaffari <gh.msaeed@gmail.com>
2023-06-05 15:08:02 -0400

    add split_string function

--- main.cpp
+++ main.cpp
@@ -200,6 +200,18 @@ public:
 
 };
 
+vector<string> split_string(const string &str, char delimiter);
+
 int main() {
     return 0;
+}
+
+vector<string> split_string(const string &str, char delimiter) {
+    vector<string> result;
+    stringstream ss(str);
+    string token;
+    while (getline(ss, token, delimiter)) {
+        result.push_back(token);
+    }
+    return result;
 }
